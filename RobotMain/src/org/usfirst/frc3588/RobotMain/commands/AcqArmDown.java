e5984a8c07543fcfff36b4d6c7e7bb917664fcfe
Will Nelson <nelson.will@live.com>
2016-02-17 09:48:54 -0800

    Feb 16 Update

--- RobotMain/src/org/usfirst/frc3588/RobotMain/commands/AcqArmDown.java
+++ RobotMain/src/org/usfirst/frc3588/RobotMain/commands/AcqArmDown.java
@@ -43,7 +43,7 @@ public class AcqArmDown extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	RobotMap.acqArmRotate.set(-.5);
+    	RobotMap.acqArmRotate.set(.75);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -53,10 +53,12 @@ public class AcqArmDown extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	RobotMap.acqArmRotate.set(0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
--- RobotMain/src/org/usfirst/frc3588/RobotMain/commands/DefenseDown.java
+++ RobotMain/src/org/usfirst/frc3588/RobotMain/commands/DefenseDown.java
@@ -43,7 +43,7 @@ public class DefenseDown extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	RobotMap.defenseArm.set(-.5);
+    	RobotMap.defenseArm.set(-.1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
